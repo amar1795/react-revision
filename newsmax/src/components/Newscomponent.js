cba5f35697947e8dc79b33a82ca9aa1087970d35
Amar <jeetamar496@gmail.com>
2023-07-10 23:26:54 +0530

    populating dom using map function

--- newsmax/src/components/Newscomponent.js
+++ newsmax/src/components/Newscomponent.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export default class Newscomponent extends Component {
   render() {
 
-    let {title,description}=this.props
+    let {title,description,imageUrl,newsUrl}=this.props
     return (
         <div>
         <div class="card" style={{width: "18rem;"}}>
-    <img src="..." class="card-img-top" alt="..."/>
+    <img src={imageUrl} class="card-img-top" alt="..."/>
     <div class="card-body">
       <h5 class="card-title">{title}</h5>
       <p class="card-text">{description}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href={newsUrl} target='_blank' class="btn btn-primary">Go somewhere</a>
     </div>
   </div>
       </div>
