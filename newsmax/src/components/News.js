1be14a9d085361846519d72bc0735b859e7aa9e4
Amar <jeetamar496@gmail.com>
2023-07-14 23:40:30 +0530

    infinite scroll added

--- newsmax/src/components/News.js
+++ newsmax/src/components/News.js
@@ -9,24 +9,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 
 export default class News extends Component {
-    articles=[
-
-        {
-            "source": {
-                "id": null,
-                "name": "POLITICO.eu"
-            },
-            "author": "Hans von der Burchard, Paul McLeary, Laura Kayali",
-            "title": "Western powers race to finish security pledges for Ukraine - POLITICO Europe",
-            "description": "The US, Britain, France and Germany are hoping to unveil a framework others can join at a NATO summit this week.",
-            "url": "https://www.politico.eu/article/western-powers-nato-security-pledges-ukraine-putin-russia-war/",
-            "urlToImage": "https://www.politico.eu/cdn-cgi/image/width=1200,height=630,fit=crop,quality=80,onerror=redirect/wp-content/uploads/2023/05/30/GettyImages-1242052307-scaled.jpg",
-            "publishedAt": "2023-07-09T13:54:10Z",
-            "content": "Press play to listen to this article\r\nVoiced by artificial intelligence.\r\nA small group of Western allies are engaged in advanced and frantic, last-minute negotiations to finalize a security assuranc… [+5581 chars]"
-        },
-
-      
-    ]
+    articles=[]
 
     static defaultProps={
 
@@ -38,8 +21,6 @@ export default class News extends Component {
     static propTypes={
        
         pagesize:PropTypes.number,
-
-
     }
 
 
@@ -58,15 +39,13 @@ export default class News extends Component {
     async updateNews(){
 
         let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4136a686f9784b0cae639181c30d9814&page=${this.state.page}&pagesize=${this.props.pagesize}`
-        this.setState({
-            image:true
-        })
+       
         let data=await fetch(url);
         let parseddata=await data.json();
         this.setState({
         articles:parseddata.articles,
         totalResults:parseddata.totalResults,
-        image:false
+        
 
         })
     }
@@ -111,6 +90,7 @@ export default class News extends Component {
                 let data=await fetch(url);
                 let parseddata=await data.json();
                 this.setState({
+                    // concatenating the data for the infinite loading bar to work 
                 articles:this.state.articles.concat(parseddata.articles),
                 totalResults:parseddata.totalResults,
                 image:false,
@@ -132,10 +112,8 @@ export default class News extends Component {
             
 
             {/* populating the dom using map function */}
-            <div className="container d-flex justify-content-center ">
-                {/* if loading then show loading image */}
+            {/* top loader only displaayed while reloading */}
             {this.state.image && <Loading/>}
-            </div>
 
             {/* if not loading then show loading image */}
 
@@ -143,11 +121,12 @@ export default class News extends Component {
           dataLength={this.state.articles.length}
           next={this.fetchMoreData}
           hasMore={true}
+        //   this loading will be shown at the time of loading down
           loader={<Loading/>}
                             >
          <div className='row'>
             {this.state.articles.map((element)=>{
-            console.log(    "this is working")
+            console.log("this is working")
             return  <div className='col-md-3 my-3' key={element.url}  >
                 {/* was unable to add slice in description getting error needs to be corrected 
                 the error was because few value of description are being set to null*/}
@@ -155,7 +134,7 @@ export default class News extends Component {
             <Newscomponent title={element.title?element.title.slice(0,45):" "} description={element.description ? element.description.slice(0, 80) : ''} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
             </div>
             })} 
-            </div>
+        </div>
 
             
             </InfiniteScroll> 
