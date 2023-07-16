5fa4804a399028d2f5a3e0473afafe31268626df
Amar <jeetamar496@gmail.com>
2023-07-17 01:27:36 +0530

    try can catch implemented with express validator

--- notemax/backend/routes/auth.js
+++ notemax/backend/routes/auth.js
@@ -8,17 +8,31 @@ router.post('/',[
     body('name').isLength({ min: 3 }).withMessage("enter a valid name"),
     body("email","enter a valid email").isEmail(),
     body('password',"enter a valid password").isLength({ min: 5 }),
-  ],(req,res)=>{
+  ],async (req,res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ error: errors.array() });  
     }
-    User.create({
+
+    try{
+        let user=await User.findOne({email:req.body.email})
+
+        if(user){
+            return res.status(400).json({error:"sorry a user exist with this email already"})
+        }
+    
+         result=await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then(user => res.json(user)).catch(err=>{console.log(err)
-        res.json({error:"please enter a valid email",message:err.message})})
+      })
+      res.json(result)
+      }
+      catch(error){
+        console.error(error.message)
+        res.status(500).json("some error occured")
+
+      }
 })
 
 
