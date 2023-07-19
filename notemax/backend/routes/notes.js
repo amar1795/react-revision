9faa70b2a0b627bfbfd5402ee3e7147c839a1d13
Amar <jeetamar496@gmail.com>
2023-07-19 17:53:34 +0530

    delete api created

--- notemax/backend/routes/notes.js
+++ notemax/backend/routes/notes.js
@@ -59,8 +59,11 @@ router.put('/updatenote/:id',fetchuser,async(req,res)=>{
     //using destructuring to store the values 
     const {title,description,tag}=req.body;
     
-        //creating a newnoteobject
-        const newnote={};
+        try {
+            //creating a newnoteobject
+        const newnote={ };
+        // below also works fine
+        // const newnote={ title,description,tag};
 
         if(title){newnote.title=title}
         if(description){newnote.description=description}
@@ -80,7 +83,38 @@ router.put('/updatenote/:id',fetchuser,async(req,res)=>{
         //if its the user whose notes are then they can update the notes
         note=await Notes.findByIdAndUpdate(req.params.id,{$set:newnote},{new:true})
         res.json({note});
+            
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).json("some error occured")   
+            
+        }
+})
 
+//ROUTE 4:Deleting all the notes using :DELETE /api/notes/delete/id user need to logged in already 
 
+router.delete('/deletenote/:id',fetchuser,async(req,res)=>{
+    
+        try {
+            //find the note to be updated and udpate it  ,req.params.id will fetch the ID from parameter in the link
+        let note=await Notes.findById(req.params.id);
+
+        //if note is not there in the link
+        if(!note){return res.status(404).send("not found")}
+
+        //if the note is not of the user whose note is then they are note allowed to update it 
+        if(note.user.toString()!==req.user.id){
+            return res.status(401).send("not allowed")
+        }
+
+        //if its the user whose notes are then they can update the notes
+        note=await Notes.findByIdAndDelete(req.params.id)
+        res.json({sucess:"your note has been deleted", note});
+            
+        } catch (error) {
+        console.error(error.message)
+        res.status(500).json("some error occured")   
+            
+        }
 })
 module.exports=router;
