78efb47fb94f16566cda2df5cded7b89eceb34cc
Amar <jeetamar496@gmail.com>
2023-07-19 17:13:06 +0530

    updating the notes api done

--- notemax/backend/routes/notes.js
+++ notemax/backend/routes/notes.js
@@ -5,28 +5,23 @@ const fetchuser = require('../middleware/fetchuser');
 const Notes=require('../models/Notes')
 const { body, validationResult } = require('express-validator');
 
-
-
-
-
-//ROUTE 1:get all the notes using :GET //api/notes/fetchallnotes login required
+//ROUTE 1:get all the notes using :GET //api/notes/fetchallnotes need to be logedin already
 
 router.get('/fetchallnotes',fetchuser,async(req,res)=>{
 
     try {
+        //req.user is from fetcher user middleware
         const notes=await Notes.find({user:req.user.id})
         res.json(notes);
         
     } catch (error) {
         console.error(error.message)
-        res.status(500).json("some error occured")
-
-        
+        res.status(500).json("some error occured")   
     }
    
 })
 
-//ROUTE 2:add all the notes using :GET /api/notes/addnote login required
+//ROUTE 2:add all the notes using :GET /api/notes/addnote user need to logged in already 
 
 router.post('/addnote',fetchuser,[
     body('title',"Enter a valid title").isLength({ min: 3 }),
@@ -34,7 +29,6 @@ router.post('/addnote',fetchuser,[
     ],async(req,res)=>{
 
         try {
-
             //fetchesthe below value from fetchuser middleware
             const {title,description,tag}=req.body;
         
@@ -42,26 +36,51 @@ router.post('/addnote',fetchuser,[
             if (!errors.isEmpty()) {
           return res.status(400).json({ error: errors.array() });  
             }
-    
+
+            //creating note object to add new notes
             const note=new Notes({
+                //user id is coming from fetch user js to verify if its the same user who is logged in
              title,description,tag,user:req.user.id
             })
             const savedNote=await note.save();
-    
             res.json(savedNote)
         } catch (error) {
             console.error(error.message)
-            res.status(500).json("some error occured")
-
-            
+            res.status(500).json("some error occured")   
         }
-
-      
-
 
 })
 
 
+//ROUTE 3:Updating  all the notes using :PUT /api/notes/updatenote/id user need to logged in already 
+
+router.put('/updatenote/:id',fetchuser,async(req,res)=>{
+
+    //using destructuring to store the values 
+    const {title,description,tag}=req.body;
+    
+        //creating a newnoteobject
+        const newnote={};
+
+        if(title){newnote.title=title}
+        if(description){newnote.description=description}
+        if(tag){newnote.tag=tag}
+
+        //find the note to be updated and udpate it  ,req.params.id will fetch the ID from parameter in the link
+        let note=await Notes.findById(req.params.id);
+
+        //if note is not there in the link
+        if(!note){return res.status(404).send("not found")}
+
+        //if the note is not of the user whose note is then they are note allowed to update it 
+        if(note.user.toString()!==req.user.id){
+            return res.status(401).send("not allowed")
+        }
+
+        //if its the user whose notes are then they can update the notes
+        note=await Notes.findByIdAndUpdate(req.params.id,{$set:newnote},{new:true})
+        res.json({note});
 
 
+})
 module.exports=router;
