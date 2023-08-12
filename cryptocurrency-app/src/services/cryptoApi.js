ccf1250ebc6004400f293dc91d7ed152bf932dd8
Amar <jeetamar496@gmail.com>
2023-08-12 16:45:21 +0530

    Cannot convert undefined or null to object redux toolkit error corrected

--- cryptocurrency-app/src/services/cryptoApi.js
+++ cryptocurrency-app/src/services/cryptoApi.js
@@ -16,6 +16,7 @@ export const cryptoApi=createApi({
     endpoints:(builder)=>({
         getCryptos:builder.query({
             query:()=>createRequest(`/coins`)
+
             // query:()=>`/coins`
 
         })
@@ -27,29 +28,3 @@ export const cryptoApi=createApi({
 
 
 
-// const axios = require('axios');
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://coinranking1.p.rapidapi.com/coins',
-//   params: {
-//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//     timePeriod: '24h',
-//     'tiers[0]': '1',
-//     orderBy: 'marketCap',
-//     orderDirection: 'desc',
-//     limit: '50',
-//     offset: '0'
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': '5993f902b8mshc12c46803594a66p12c663jsncc7d851ba210',
-//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//   }
-// };
-
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// }
