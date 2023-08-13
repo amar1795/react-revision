07c719a2ccf60b19565a5261ba301c92a60a615c
Amar <jeetamar496@gmail.com>
2023-08-13 19:49:00 +0530

    search option added in news component for crypto

--- cryptocurrency-app/src/components/News.jsx
+++ cryptocurrency-app/src/components/News.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Select,Typography,Row,Col,Avatar,Card } from 'antd';
 import moment from 'moment';
-
+import { useGetCryptosQuery } from '../services/cryptoApi';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
-const demoImage = ' '
-
+import demoImage from "../images/bitcoin-2007769_640.jpg"
 const {Text,Title}=Typography;
 const {Option}=Select;
 
 const News = ({simplified}) => {
+  
   const count=simplified?6:12;
-  const {data:cryptoNews,isFetching} =useGetCryptoNewsQuery({newsCategory:'Cryptocurrency',count})
+  // usestate to set the newscategory manualy using the search and default is cryptocurency
+  const [newsCategory,setnewsCategory]=useState('Cryptocurrency')
+  const {data:cryptoNews,isFetching} =useGetCryptoNewsQuery({newsCategory,count})
+  const {data:cryptosList}= useGetCryptosQuery(100);
   console.log(cryptoNews)
   if(isFetching) return "Loading..."
 
   return (
-    
-
     <Row gutter={[24,24]}>
+      {!simplified &&(
+        <Col span={24}>
+          <Select
+            showSearch
+            className='select-news'
+            placeholder="select a crypto"
+            optionFilterProp='children'
+            onChange={(value)=>setnewsCategory(value)}
+            filterOption={(input,option)=>option.children.toLowerCase().indexOf(input.toLowerCase()>=0)}
+            >
+              <option value="Cryptocurrency">Cryptocurrency</option>
+              {/* iterating through the available options of crypto that will be shown in the options */}
+              {cryptosList?.data?.coins.map((coin)=><Option value={coin.name}>{coin.name}</Option>)}
+
+            </Select>
+        </Col>
+      )}
       {cryptoNews?.value?.map((news,i)=>(
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className='news-card'>
@@ -27,7 +45,7 @@ const News = ({simplified}) => {
                   <Title className="news-title" level={4}>
                     {news.name}     
                   </Title>
-                  <img src={news?.image?.thumbnail?.contentUrl || demoImage } alt="news" />
+                  <img style={{maxWidth:"100px",maxHeight:"100px"}} src={news?.image?.thumbnail?.contentUrl || demoImage } alt="news" />
               </div>
                 <p>
                   {news.description>100 ? `${news.description.subsctring(0,100)}...`: news.description}
