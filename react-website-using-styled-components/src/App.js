870deb8d32a1b32658af074920e168a362692d15
Amar <jeetamar496@gmail.com>
2023-08-14 23:15:17 +0530

    features page done

--- react-website-using-styled-components/src/App.js
+++ react-website-using-styled-components/src/App.js
@@ -1,32 +1,49 @@
-import { styled } from 'styled-components';
+import { styled,css } from 'styled-components';
 import './App.css';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
+import Feature from './components/Feature';
 
 const Container=styled.div`
 height: 100vh;
 overflow: hidden;
+position: relative;
 /* background-color: black ; */
 `
-const IntroSec=styled.div`
-clip-path: polygon(100% 0%, 100% 48%, 100% 100%, 60% 100%, 50% 49%, 74% 0);background-color: blueviolet;
+
+const Shape = css`
 width: 100%;
 height: 100%;
 position: absolute;
 top: 0%;
 left: 0%;
 z-index: -1;
+`
+const IntroSec=styled.div`
+${Shape}
+clip-path: polygon(100% 0%, 100% 48%, 100% 100%, 60% 100%, 50% 49%, 74% 0);background-color: blueviolet;
 overflow: hidden;
 
 `
+const FeatureSec=styled.div`
+clip-path: polygon(0% 0%, 31% 0, 47% 50%, 30% 100%, 0% 100%);background-color: blueviolet;
+${Shape}
+overflow: hidden;
+`
 
 function App() {
-  return (
+  return (<>
    <Container>
   <Navbar/>
   <Intro/>  
   <IntroSec/>
    </Container>
+
+   <Container>
+    <Feature/>
+    <FeatureSec/>
+   </Container>
+   </>
   );
 }
 
---
+++ react-website-using-styled-components/src/components/Feature.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import img from '../images/app_phone.png'
+
+
+
+
+import { styled } from 'styled-components'
+const Container= styled.div`
+    height:100vh;
+    display: flex;
+  
+`
+const Left=styled.div`
+/* background-color: beige; */
+    width: 50%;
+`
+const Image=styled.img`
+
+margin-top: 10px;
+    width: 80%;
+`
+const Right=styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+/* background-color: #0482c6; */
+
+    width: 50%;
+`
+const Title=styled.span`
+font-size: 70px;
+
+`
+const Subtitle=styled.span`
+font-size: 24px;
+font-style:italic;
+font-weight: 100;
+margin-top: 20px;
+
+`
+const Description=styled.p`
+font-size: 20px;
+margin-top: 30px;
+
+
+`
+const Button=styled.button`
+font-size: 20px;
+padding: 15px 20px;
+font-weight: bold;
+cursor: pointer;
+margin-top: 20px;
+width: 150px;
+border-radius: 20px;
+border: none;
+background-color:blueviolet;
+`
+
+
+
+
+
+
+
+const Feature = () => {
+  return (
+    <Container>
+        <Left>
+            <Image src={img} >
+
+            </Image>
+
+        </Left>
+        <Right>
+            <Title>
+                <b>Good </b>Design
+                <br />
+                <b>Good </b>Business
+
+            </Title>
+            <Subtitle>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, consequuntur.
+
+            </Subtitle>
+            <Description>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita vel dolor dolore error, assumenda inventore debitis deserunt tempora, voluptatum quam animi et minima saepe? Earum.</p>   
+            </Description>
+            <Button>
+                Learn More
+            </Button>
+        </Right>
+      
+    </Container>
+  )
+}
+
+export default Feature
