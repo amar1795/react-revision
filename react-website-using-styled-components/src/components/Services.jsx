8382af8fbb79238939665ef1a7adefc5fea6fa74
Amar <jeetamar496@gmail.com>
2023-08-15 21:24:06 +0530

    video added

--- react-website-using-styled-components/src/components/Services.jsx
+++ react-website-using-styled-components/src/components/Services.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { styled } from 'styled-components'
 import women from '../images/pngwing.com(1).png'
 import Minicard from './Minicard'
@@ -9,29 +9,39 @@ display: flex;
 `
 const Left=styled.div`
     width: 50%;
+    position: relative;
     
     /* background-color: bisque; */
 `
 const Image=styled.img`
     height: 100%;
-    
-    /* overflow: hidden; */
+    display: ${(props)=> props.open && "none"}
+ 
 `
+const Video = styled.video`
+  display: ${(props) => !props.open && "none"};
+  height: 55%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 30px;
+  margin: auto;
+  border-radius: 20px;
+  /* video was not being displayed because of the not supported video link */
+`;
+
+
 const Right=styled.div`
     width: 50%;
     /* background-color: aquamarine; */
 `
 const Wrapper=styled.div`
 width: 100%;
-
-    
 `
 const Title=styled.h1`
 margin-top: 40px;
 padding: 20px;
 font-size: 50px;
-
-    
 `
 const Description=styled.p`
 font-size: 25px;
@@ -51,17 +61,21 @@ const Button=styled.button`
 padding: 20px 30px;
 font-size: 20px;
 border-radius: 20px;
-margin: 20px;
+margin: 40px 20px;
 border: none;
 background-color: blueviolet;
 color: white;
     cursor: pointer;
 `
 const Services = () => {
+    const [open,setopen]=useState(false)
+    /* above usestate will be used in styled components */
   return (
     <Container>
         <Left>
-            <Image src={women} />
+            <Image open={open} src={women} />
+            <Video open={open} autoPlay loop src='https://player.vimeo.com/external/449759244.sd.mp4?s=d5f3da46ddc17aa69a7de84f1e420610ebd2a391&profile_id=139&oauth2_token_id=57447761'
+            />
         </Left>
         <Right>
             <Wrapper>
@@ -76,7 +90,7 @@ const Services = () => {
                     <Minicard></Minicard>
                     <Minicard></Minicard>
                 </Cardcontainer>
-                <Button>
+                <Button onClick={()=>setopen(!open)}>
                     How it Works
                 </Button>
             </Wrapper>
