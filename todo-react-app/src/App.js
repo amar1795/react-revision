154b62d573a2f09664c5a5148f7e03ee11ca4843
Amar <jeetamar496@gmail.com>
2023-08-19 01:19:38 +0530

    form submmit and data populating via map function added

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,18 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Checkbox from './components/Checkbox';
 import Inputform from './components/Inputform';
 
 function App() {
+  const [task,settask]=useState([])
+  
+  function addtask(name) {
+    settask(prev=>{
+      return [...prev,{name:name,done:false}]
+    })
+    
+  }
+
   return (
     <main >
-      <Inputform/>
-      <Checkbox/>
-      <Checkbox/>
-      <Checkbox/>
-      <Checkbox/>
-      <Checkbox/>
-
-
+      <Inputform add={addtask}/>
+      {/* here the add prop value is coming from input form , here the prop value is goin in oppposite direction */}
+      
+      {task.map(e=>(
+        <Checkbox defaultchecked={task.done} {...e}/>
+      ))}
+        
     </main>
   );
 }
--- todo-react-app/src/components/Inputform.jsx
+++ todo-react-app/src/components/Inputform.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Inputform = () => {
+const Inputform = ({add}) => {
+    const [formvalue,setformvalue] =useState("")
+     
+    function handlesubmit(e) {
+        e.preventDefault();
+        add(formvalue);
+        setformvalue(" ")
+        
+    }
+
   return (
-    <form>
+    <form onSubmit={handlesubmit}>
         <button>+</button>
-        <input type="text" placeholder='Enter your task' />
-      
+        <input type="text" placeholder='Enter your task' value={formvalue} onChange={e=>setformvalue(e.target.value)} />
+        
     </form>
   )
 }
