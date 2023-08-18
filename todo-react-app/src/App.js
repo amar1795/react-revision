6a4618a8d4ec1daf0b9f46b90dba6fa4b27115b7
Amar <jeetamar496@gmail.com>
2023-08-19 02:57:29 +0530

    checkbox functionality added

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,25 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Checkbox from './components/Checkbox';
 import Inputform from './components/Inputform';
+import Task from './components/Task';
 
 function App() {
   const [task,settask]=useState([])
   
   function addtask(name) {
     settask(prev=>{
+      // here ..prev is a spread operator and takes all the previous values of the array and create a new array with the name and done:false
       return [...prev,{name:name,done:false}]
     })
     
   }
+
+  useEffect(()=>{
+    if(task.length ===0) return;
+    localStorage.setItem('notetask',JSON.stringify(task))
+  },[task])
+
+  useEffect(()=>{
+    const notevalue=JSON.parse(localStorage.getItem('notetask'))
+    settask(notevalue);
+  },[])
+
+  function updatetask(taskindex,newdone) {
+    settask(prev=>{
+      const newtask=[...prev];
+      newtask[taskindex].done= newdone;
+      return newtask;
+    })
+    
+  }
+  
 
   return (
     <main >
       <Inputform add={addtask}/>
       {/* here the add prop value is coming from input form , here the prop value is goin in oppposite direction */}
       
-      {task.map(e=>(
-        <Checkbox defaultchecked={task.done} {...e}/>
+      {task.map((task,index)=>(
+       <Task {...task} onToggle={done=>updatetask(index,done)}/>
       ))}
         
     </main>
---
+++ todo-react-app/src/components/Task.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Checkbox from './Checkbox'
+
+const Task = ({name,done,onToggle}) => {
+  return (
+    <div className='task'>
+       <Checkbox checked={done} onClick={()=>onToggle(!done)}  />
+       {name}
+    </div>
+  )
+}
+
+export default Task
