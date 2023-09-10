824c73e5668ebc8ec6741c891a0fc8e767a8d23f
Amar <jeetamar496@gmail.com>
2023-09-10 18:30:11 +0530

    basic navbar template created

---
+++ Netflix Mern Stack Clone/src/home/Home.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+// no need to import react after react 18 
+import Navbar from "../components/navbar/Navbar"
+import "./home.scss"
+
+const Home = () => {
+  return (
+    <div className="home">
+    <Navbar/>
+    </div>
+  )
+}
+
+export default Home
