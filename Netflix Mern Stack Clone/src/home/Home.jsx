97718a21b376322530e773feeb82aefdbb889e9a
Amar <jeetamar496@gmail.com>
2023-09-11 23:29:50 +0530

    featured section added and genre option added

--- Netflix Mern Stack Clone/src/home/Home.jsx
+++ Netflix Mern Stack Clone/src/home/Home.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 // no need to import react after react 18 
+import Featured from "../components/featured/Featured"
 import Navbar from "../components/navbar/Navbar"
 import "./home.scss"
 
@@ -7,6 +8,8 @@ const Home = () => {
   return (
     <div className="home">
     <Navbar/>
+
+    <Featured type={"series"}/>
     </div>
   )
 }
