16693c6a67a1b0f65fe773810b8e78acb1ae631d
Amar <jeetamar496@gmail.com>
2023-09-12 23:58:33 +0530

    useref created to move the slider left and right using get boundingclient rect

--- Netflix Mern Stack Clone/src/components/list/List.jsx
+++ Netflix Mern Stack Clone/src/components/list/List.jsx
@@ -1,14 +1,25 @@
 import { ArrowBackIosOutlined, ArrowForwardIos, ArrowForwardIosOutlined } from '@mui/icons-material'
-import React from 'react'
+import React, { useRef } from 'react'
 import ListItem from '../ListItem/ListItem'
 import "./list.scss"
 const List = () => {
+    const listRef=useRef()
+
+    const handleClick=(direction)=>{
+         let distance=listRef.current.getBoundingClientRect().x-50;
+
+        if(direction ==="left")
+        {
+            listRef.current.style.transform=`translateX(${230+distance}px)`
+        }
+        
+    }
   return (
     <div className='list'>
         <span className='listTitle'>Continue to watch</span>
         <div className="wrapper">
-            <ArrowBackIosOutlined/>
-            <div className="container">
+            <ArrowBackIosOutlined className='sliderArrow Left' onClick={()=>handleClick("left")} />
+            <div className="container" ref={listRef}>
                 <ListItem/>
                 <ListItem/>
                 <ListItem/>
@@ -21,22 +32,10 @@ const List = () => {
                 <ListItem/>
                 <ListItem/>
                 <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
-                <ListItem/>
+              
                 
             </div>
-            <ArrowForwardIosOutlined/>
+            <ArrowForwardIosOutlined className='sliderArrow Right'onClick={()=>handleClick("right")}/>
         </div>
      
     </div>
