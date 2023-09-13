0f3dfce6be88001e4c5c83f0b083333474f799be
Amar <jeetamar496@gmail.com>
2023-09-14 01:38:34 +0530

    login page created

--- Netflix Mern Stack Clone/src/App.js
+++ Netflix Mern Stack Clone/src/App.js
@@ -2,13 +2,15 @@ import './App.scss';
 import Home from './pages/home/Home';
 import Register from './pages/register/Register';
 import Watch from './pages/watch/Watch';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
      {/* <Home/> */}
      {/* <Watch/> */}
-     <Register/>
+     {/* <Register/> */}
+     <Login/>
     </div>
   );
 }
