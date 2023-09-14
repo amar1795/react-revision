bf65fe2b0fab320c387c22435d75f2dd941a2f40
Amar <jeetamar496@gmail.com>
2023-09-14 20:57:15 +0530

    db user registration read and write working on momngodb atlas

--- Netflix Mern Stack Clone/api/index.js
+++ Netflix Mern Stack Clone/api/index.js
@@ -2,6 +2,8 @@ const express =require("express");
 const app =express();
 const mongoose = require('mongoose');
 const dotenv=require("dotenv")
+const authroute=require("./routes/auth")
+
 
 dotenv.config();
 
@@ -9,6 +11,13 @@ mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=> console.log("DB Connection is Successfull")).catch((error)=>console.log(error))
+
+
+app.use(express.json())
+
+
+app.use("/api/auth",authroute)
+
 
 
 app.listen(8000,()=>{
