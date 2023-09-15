aa3b370053e5b638786a9d7a6faaa4e7c9f28105
Amar <jeetamar496@gmail.com>
2023-09-15 13:52:35 +0530

    other crud application including stats , find all user, single user created

--- Netflix Mern Stack Clone/api/routes/users.js
+++ Netflix Mern Stack Clone/api/routes/users.js
@@ -7,7 +7,7 @@ const verifyToken=require("../verifyToken")
 // update
 router.put("/:id",verifyToken,async (req,res)=>{
     
-    if(req.user.id===req.params.id || req.user.admin)
+    if(req.user.id===req.params.id || req.user.isAdmin)
     {
         // if updating the password then we will encrypt it again
         if(req.body.password){
@@ -28,18 +28,111 @@ router.put("/:id",verifyToken,async (req,res)=>{
             res.status(500).json(error)
             
         }
-
     }
-
     else{
         res.status(403).json("you can only update your account details")
     }
 
 })
 // delete
+router.delete("/:id",verifyToken,async (req,res)=>{   
+    if(req.user.id===req.params.id || req.user.isAdmin)
+    {       
+        try {
+            await User.findByIdAndDelete(req.params.id,)
+            res.status(200).json("user has been deleted");
+        } catch (error) {
+            res.status(500).json(error)           
+        }
+    }
+    else{
+        res.status(403).json("you can only delete your account ")
+    }
+})
 // get
+
+
+// we don't need to verify the jwt token to search for the users
+router.get("/find/:id",async (req,res)=>{   
+    try {
+        const user=await User.findById(req.params.id,)
+        const {password,...info}=user._doc;
+
+        res.status(200).json(info);
+    } catch (error) {
+        res.status(500).json(error)           
+    }
+})
+
+
 // getall
+
+router.get("/",verifyToken,async (req,res)=>{   
+    const query=req.query.new;
+    if(req.user.isAdmin)
+    {       
+        try {
+            const users=query? await User.find().sort({_id:-1}).limit(5) :await User.find();
+            res.status(200).json(users);
+        } catch (error) {
+            res.status(500).json(error)           
+        }
+    }
+    else{
+        res.status(403).json("you are not allowed to see all users")
+    }
+})
+
 // getuserstats
+
+
+router.get("/stats",async (req,res)=>{
+    const today=new Date();
+    const latYear=today.setFullYear(today.setFullYear()-1);
+    
+    const monthsArray=[
+        "january",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "Septemeber",
+        "October",
+        "November",
+        "December",
+        
+    ];
+
+    try {
+            const data=await User.aggregate(
+                [
+                    {
+                $project:{
+                    month:{
+                        $month:"$createdAt"
+                    },
+                    }},
+                    {
+                    $group:{
+                        _id:"$month",
+                        total:{$sum:1}
+                    },
+                    },
+                          
+                ])
+
+                res.status(200).json(data)
+        }
+     catch (error) {
+        res.status(500).json(error)
+        
+    }
+
+    
+})
 
 
 module.exports=router;
