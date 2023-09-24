bbb9167697b0abe856b88ea811b3dd4b3c6166dd
Amar <jeetamar496@gmail.com>
2023-09-25 01:05:36 +0530

    deletion logic added in redux

--- expensetracker/src/components/Transaction.jsx
+++ expensetracker/src/components/Transaction.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React,{useContext, useState} from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
 
 const Transaction = ({transaction}) => {
     const sign=transaction.amount < 0 ? "-" :"+";
+    const {deleteTransaction}=useContext(GlobalContext)
 
   return (
     <div>
        <li class={transaction.amount < 0 ? "minus" :"plus"}>
-         {transaction.text}<span>${sign}{Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
+         {transaction.text}<span>${sign}{Math.abs(transaction.amount)}</span><button class="delete-btn"onClick={()=>deleteTransaction(transaction.id)} >x</button>
         </li>
     </div>
   )
