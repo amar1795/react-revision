37244e3aabebce74eb7a6a120da3e9df9cebb679
Amar <jeetamar496@gmail.com>
2023-09-29 22:48:32 +0530

    chart created

--- react_admin_dashboard/src/pages/home/Home.jsx
+++ react_admin_dashboard/src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./home.css"
 import Featuredinfo from '../../components/featuredinfo/Featuredinfo'
+import Chart from '../../components/chart/Chart'
+import { userData } from '../../Dummydata'
 const Home = () => {
   return (
     <div className='home'>
       <Featuredinfo/>
+      <Chart data={userData} grid title="User Analytics" dataKey="Active User"/>
+      <div className="homeWidgets">
+        
+      </div>
     </div>
   )
 }
